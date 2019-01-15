4a183398058fe2623dda0c187cbd5a01782fd022
Matt Voorhees <voorhees.matthew@gmail.com>
2019-01-14 17:37:32 -0800

    Add options to popup

--- js/options.js
+++ js/options.js
@@ -8,6 +8,12 @@ function constructOptions(kButtonColors) {
             chrome.storage.sync.set({color: item}, function() {
                 console.log('color is ' + item);
             });
+            let changeColor = document.getElementById('changeColor');
+
+            chrome.storage.sync.get('color', function (data) {
+                changeColor.style.backgroundColor = data.color;
+                changeColor.setAttribute('value', data.color);
+            });
         });
         page.appendChild(button);
     }
